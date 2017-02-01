186e367a3bf76757c072c98fa2a5707738d86ea0
David McClure <dmcclure@pivotal.io>
2017-02-01 12:33:06 -0800

    wip
    Signed-off-by: Joachim Valdez <valdezj@us.ibm.com>

---
+++ src/policy-server/cleaner/poller_test.go
@@ -0,0 +1,77 @@
+package cleaner_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Poller", func() {
+	Describe("Run", func() {
+		// var (
+		// 	logger  *lagertest.TestLogger
+		// 	p       *poller.Poller
+		// 	signals chan os.Signal
+		// 	ready   chan struct{}
+
+		// 	cycleCount uint64
+		// 	retChan    chan error
+		// )
+
+		BeforeEach(func() {
+			// signals = make(chan os.Signal)
+			// ready = make(chan struct{})
+
+			// cycleCount = 0
+			// retChan = make(chan error)
+
+			// logger = lagertest.NewTestLogger("test")
+
+			// p = &poller.Poller{
+			// 	Logger:       logger,
+			// 	PollInterval: 10 * time.Millisecond,
+
+			// 	SingleCycleFunc: func() error {
+			// 		atomic.AddUint64(&cycleCount, 1)
+			// 		return nil
+			// 	},
+			// }
+		})
+
+		It("calls the single cycle func", func() {
+			// go func() {
+			// 	retChan <- p.Run(signals, ready)
+			// }()
+
+			// Eventually(ready).Should(BeClosed())
+			// Eventually(func() uint64 {
+			// 	return atomic.LoadUint64(&cycleCount)
+			// }).Should(BeNumerically(">", 0))
+
+			// Consistently(retChan).ShouldNot(Receive())
+
+			// signals <- os.Interrupt
+			// Eventually(retChan).Should(Receive(nil))
+		})
+
+		Context("when the cycle func errors", func() {
+			BeforeEach(func() {
+				// p.SingleCycleFunc = func() error { return errors.New("banana") }
+			})
+
+			It("logs the error and continues", func() {
+				// go func() {
+				// 	retChan <- p.Run(signals, ready)
+				// }()
+
+				// Eventually(ready).Should(BeClosed())
+
+				// Eventually(logger).Should(gbytes.Say("poll-cycle.*banana"))
+
+				// Consistently(retChan).ShouldNot(Receive())
+
+				// signals <- os.Interrupt
+				// Eventually(retChan).Should(Receive(nil))
+			})
+		})
+	})
+})
